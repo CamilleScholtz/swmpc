979b877c52353dbc270c1b9c956a4738d15075bf
Camille Scholtz <admin@camille.sh>
2024-11-08 13:20:27 +0100

    Initial Commit

---
+++ swmpc/swmpcApp.swift
@@ -0,0 +1,17 @@
+//
+//  swmpcApp.swift
+//  swmpc
+//
+//  Created by Camille Scholtz on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct swmpcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
