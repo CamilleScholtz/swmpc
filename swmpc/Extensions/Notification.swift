f2a4c459e493223ccd3f6e9cc2ec6a945ddd54b7
Camille Scholtz <admin@camille.sh>
2024-12-31 15:47:21 +0100

    Add extension

---
+++ swmpc/Extensions/Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.swift
+//  swmpc
+//
+//  Created by Camille Scholtz on 22/12/2024.
+//
+
+import SwiftUI
+
+extension Notification.Name {
+    static let scrollToCurrentNotification = Notification.Name("scrollToCurrent")
+    static let startSearchingNotication = Notification.Name("startSearching")
+}
