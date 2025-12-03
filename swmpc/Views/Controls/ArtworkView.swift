b019f06928891b678de20d6084d98089db8549af
Camille Scholtz <admin@camille.sh>
2025-12-03 23:00:41 +0100

    Add a demo mode

--- swmpc/Views/Controls/ArtworkView.swift
+++ swmpc/Views/Controls/ArtworkView.swift
@@ -18,10 +18,22 @@ struct ArtworkView: View {
                     Image(uiImage: image)
                         .resizable()
                         .aspectRatio(contentMode: aspectRatioMode)
+                    #if DEMO
+                        .blur(radius: 8)
+                        .overlay {
+                            GrainOverlay()
+                        }
+                    #endif
                 #elseif os(macOS)
                     Image(nsImage: image)
                         .resizable()
                         .aspectRatio(contentMode: aspectRatioMode)
+                    #if DEMO
+                        .blur(radius: 8)
+                        .overlay {
+                            GrainOverlay()
+                        }
+                    #endif
                 #endif
             } else {
                 Rectangle()
@@ -30,4 +42,25 @@ struct ArtworkView: View {
             }
         }
     }
+
+    #if DEMO
+        private struct GrainOverlay: View {
+            var body: some View {
+                Canvas { context, size in
+                    for _ in 0 ..< Int(size.width * size.height * 0.5) {
+                        let x = Double.random(in: 0 ..< size.width)
+                        let y = Double.random(in: 0 ..< size.height)
+                        let opacity = Double.random(in: 0.05 ... 0.15)
+
+                        context.fill(
+                            Path(ellipseIn: CGRect(x: x, y: y, width: 1, height: 1)),
+                            with: .color(.white.opacity(opacity)),
+                        )
+                    }
+                }
+                .blendMode(.overlay)
+                .allowsHitTesting(false)
+            }
+        }
+    #endif
 }
